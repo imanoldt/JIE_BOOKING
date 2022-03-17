67e0cf6c3ca0866686e7826bedc940d77f939115
imanoldt <imanoldt@pc-3-210.customer.ask4.lan>
2022-03-17 18:15:36 +0100

    Creacion de estuctura (Prueba)
    Signed-off-by: imanoldt

---
+++ JIE_Booking/src/Hotel.h
@@ -0,0 +1,21 @@
+/*
+ * Hotel.h
+ *
+ *  Created on: Mar 17, 2022
+ *      Author: imanoldt
+ */
+
+#ifndef HOTEL_H_
+#define HOTEL_H_
+
+typedef struct{
+	char nombre [20];
+	char descripcion[50];
+	char localidad[15];	/* codigo de hotel compuesto por dos letras y 6 numeros */
+	char codigo[9];
+	int numHabitaciones;
+}tHotel;
+
+
+
+#endif /* HOTEL_H_ */
