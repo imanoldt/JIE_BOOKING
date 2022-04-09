dedef0727ad79165693a7f734d0f6fdd53d1b1d8
usuario <usuario@MSI>
2022-04-09 12:30:55 +0200

    cliente estructura

--- JIE_Booking/src/cliente.c
+++ JIE_Booking/src/cliente.c
@@ -8,6 +8,31 @@
 
 #include "cliente.h"
 #include <stdio.h>
-#include <stdlib.h>
 
+void pedirCliente (tCliente * cl){
 
+		printf("Nombre de usuario: ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(cl->nombreUsu);
+		printf("DNI: ");
+		fflush(stdout);
+		fflush(stdin);
+		scanf("%d",&cl->DNI);
+		printf("Email: ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(cl->email);
+		printf("Año nacimiento: ");
+		fflush(stdout);
+		fflush(stdin);
+		scanf("%d", &cl->AnoNac);
+		printf("contrasena: ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(cl->contrasena);
+}
+
+void imprimirCliente(tCliente cl){
+	printf("%s %d %s %d %s\n", cl.nombreUsu, cl.DNI, cl.email, cl.AnoNac, cl.contrasena);
+}
